b1ee98dfeb5f83314ce1db1920285375385c0a89
党威 <1357711677@qq.com>
2020-04-18 10:06:21 +0800

    2020.4.18

---
+++ Test.java
@@ -0,0 +1,74 @@
+给你 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai) 。在坐标内画 n 条垂直线，垂直线 i 的两个端点分别为 (i, ai) 和 (i, 0)。找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/container-with-most-water
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+class Solution {
+    public int maxArea(int[] height) {
+        int max=0;
+        int left=0;
+        int right=height.length-1;
+        while(left<right){
+            max=Math.max(max,(right-left)*Math.min(height[left],height[right]));
+            if(height[left]<height[right]){
+                left++;
+            }else{
+                right--;
+            }
+        }
+        return max;
+    }
+}
+
+给定一个包含 0 和 1 的二维网格地图，其中 1 表示陆地 0 表示水域。
+
+网格中的格子水平和垂直方向相连（对角线方向不相连）。整个网格被水完全包围，但其中恰好有一个岛屿（或者说，一个或多个表示陆地的格子相连组成的岛屿）。
+
+岛屿中没有“湖”（“湖” 指水域在岛屿内部且不和岛屿周围的水相连）。格子是边长为 1 的正方形。网格为长方形，且宽度和高度均不超过 100 。计算这个岛屿的周长。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/island-perimeter
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+class Solution {
+    public int islandPerimeter(int[][] grid) {
+        int res=0;
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]==1){
+                    res+=4;
+                    if(i>0&&grid[i-1][j]==1) res-=2;
+                    if(j>0&&grid[i][j-1]==1) res-=2;
+                }
+            }
+        }
+        return res;
+    }
+}
+
+class Solution {
+    public int islandPerimeter(int[][] grid) {
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]==1){
+                    return dfs(grid,i,j);
+                }
+            }
+        }
+        return 0;
+    }
+    private int dfs(int[][] grid,int i,int j){
+        if(i<0||j<0||i>=grid.length||j>=grid[0].length){
+            return 1;
+        }
+        if(grid[i][j]==0){
+            return 1;
+        }
+        if(grid[i][j]==2){
+            return 0;
+        }
+
+        grid[i][j]=2;
+        return dfs(grid,i+1,j)+dfs(grid,i,j+1)+dfs(grid,i-1,j)+dfs(grid,i,j-1);
+    }
+}
+
